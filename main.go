41113813f753003aa8ed0380e898396fafcbe060
admin <77553zlp@gmail.com>
2023-05-31 16:43:56 +0800

    优化逻辑

--- main.go
+++ main.go
@@ -10,6 +10,12 @@ import (
 	"time"
 )
 
+const (
+	redColor   = "\033[31m"
+	greenColor = "\033[32m"
+	resetColor = "\033[0m"
+)
+
 func main() {
 	pathFile, err := os.OpenFile("path.txt", os.O_RDONLY, os.ModePerm)
 	if err != nil {
@@ -24,8 +30,9 @@ func main() {
 	for scanner.Scan() {
 		line := scanner.Text()
 		paths := strings.Split(line, ",")
-		if len(paths) < 2 {
-			log.Printf("%s\t路径缺失或分割错误\n", paths)
+		if len(paths) < 2 || paths[1] == "" {
+			log.Printf(redColor+"%s\t路径缺失或分割错误"+resetColor+"\n", paths)
+			break
 		}
 
 		// 使用切片定义源文件路径和目标文件路径
@@ -49,7 +56,7 @@ func main() {
 func renameFileName(filepath string) (err error) {
 
 	// 获取当前时间,并按照指定格式输出
-	currentTime := time.Now().Format("060102")
+	currentTime := time.Now().Format("060102_15_04_05")
 
 	// 设置文件名
 	fileName := fmt.Sprintf(filepath + ".bak" + currentTime)
@@ -57,9 +64,9 @@ func renameFileName(filepath string) (err error) {
 	// 进行重命名操作
 	err = os.Rename(filepath, fileName)
 	if err != nil {
-		log.Printf("%s\t备份失败: %v\n", filepath, err)
+		log.Printf(redColor+"%s\t备份失败: %v"+resetColor+"\n", filepath, err)
 	} else {
-		log.Printf("%s\t备份成功,备份名称为: %s\n", filepath, fileName)
+		log.Printf(greenColor+"%s\t备份成功,备份名称为: %s"+resetColor+" \n"+resetColor, filepath, fileName)
 	}
 	return nil
 }
@@ -69,23 +76,23 @@ func copyFile(sourceFilePath, destFilePath string) (err error) {
 	// 打开源文件
 	src, err := os.Open(sourceFilePath)
 	if err != nil {
-		log.Printf("%s\t打开失败: %v\n", sourceFilePath, err)
+		log.Printf(redColor+"%s\t打开失败: %v"+resetColor+"\n", sourceFilePath, err)
 	}
 	defer src.Close()
 
 	// 创建目标文件,先进行更名操作,更名后目标文件名已经改变,这里使用create进行创建文件
 	dst, err := os.Create(destFilePath)
 	if err != nil {
-		log.Printf("%s\t创建失败: %v\n", destFilePath, err)
+		log.Printf(redColor+"%s\t创建失败: %v"+resetColor+"\n", destFilePath, err)
 	}
 	defer dst.Close()
 
 	// 进行复制操作,将源文件内容传递至目标文件
 	_, err = io.Copy(dst, src)
 	if err != nil {
-		log.Printf("%s\t文件写入失败: %v\n", destFilePath, err)
+		log.Printf(redColor+"%s\t文件写入失败: %v"+resetColor+"\n", destFilePath, err)
 	} else {
-		log.Printf("%s\t文件写入成功", destFilePath)
+		log.Printf(greenColor+"%s\t文件写入成功"+resetColor+"\n", destFilePath)
 	}
 	return nil
 }
